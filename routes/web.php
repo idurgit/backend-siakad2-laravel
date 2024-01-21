49a5f125a6f9b90e03ff6625339856c94d149ddf
Rudi <126459042+idurgit@users.noreply.github.com>
2024-01-21 09:58:47 +0700

    crud user

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,21 +15,28 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages.app.dashboard-siakad', ['type_menu' => '']);
+    return view('pages.auth.auth-login');
 });
 
-Route::get('/login', function () {
-    return view('pages.auth.auth-login');
-})->name('login');
+Route::middleware(['auth'])->group(function() {
+    Route::get('home', function() {
+        return view('pages.app.dashboard-siakad', ['type_menu' => '']);
+    })->name('home');
+    Route::resource('user', UserController::class);
+});
 
-Route::get('/register', function () {
-    return view('pages.auth.auth-register');
-})->name('register');
+// Route::get('/login', function () {
+//     return view('pages.auth.auth-login');
+// })->name('login');
 
-Route::get('/forgot', function () {
-    return view('pages.auth.auth-forgot-password');
-})->name('forgot');
+// Route::get('/register', function () {
+//     return view('pages.auth.auth-register');
+// })->name('register');
 
-Route::get('/reset-password', function () {
-    return view('pages.auth.auth-reset-password');
-})->name('reset-password');
+// Route::get('/forgot', function () {
+//     return view('pages.auth.auth-forgot-password');
+// })->name('forgot');
+
+// Route::get('/reset-password', function () {
+//     return view('pages.auth.auth-reset-password');
+// })->name('reset-password');
